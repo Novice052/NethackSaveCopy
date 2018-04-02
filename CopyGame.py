7d9bc7b8764e95b3dc0895dda91bb3d11a4f32cc
Novice052 <immersed101@gmail.com>
2018-04-01 19:50:32 -0700

    Fixed problem with bak to sav

--- CopyGame.py
+++ CopyGame.py
@@ -4,15 +4,21 @@
 #   copy orig to bak folder
 #   copy bak to orig folder
 
-import shutil, os
+import shutil, os, re
 
 # TODO: use REGIX to find playground folder
 playground = 'C:\\Users\\immer\\AppData\\Local\\VirtualStore\\Program Files (x86)\\Nethack'
 # TODO: use REGIX to find backup folder OR create if not there
 backup = 'C:\\Users\\immer\\AppData\\Local\\VirtualStore\\Program Files (x86)\\Nethack\\Backup'
 os.chdir(playground)
-# TODO: cehck to see if file exists already, if not, don't copy.
+# TODO: check to see if file exists already, if not, don't copy.
 # TODO: use REGIX to find and copy all save files
-shutil.copy(playground + '\\immer-Dirk.NetHack-saved-game', backup)
+try:
+    shutil.copy(playground + '\\immer-Dirk.NetHack-saved-game', backup)
+except IOError:
+    print('Couldn\'t find an original save file.  Looking for a backup...' )
 # TODO: use REGIX to find all save files
-shutil.copy(backup + '\\immer-Dirk.NetHack-saved-game', playground)
+try:
+    shutil.copy(backup + '\\immer-Dirk.NetHack-saved-game', playground)
+except IOError:
+    print('No backup file found.')
