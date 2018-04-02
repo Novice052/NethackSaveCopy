4c1ea1e9d248793f44f16b988e4fd7fcbac9d451
Novice052 <immersed101@gmail.com>
2018-04-01 18:27:09 -0700

    Added TODOs

--- CopyGame.py
+++ CopyGame.py
@@ -6,8 +6,13 @@
 
 import shutil, os
 
+# TODO: use REGIX to find playground folder
 playground = 'C:\\Users\\immer\\AppData\\Local\\VirtualStore\\Program Files (x86)\\Nethack'
+# TODO: use REGIX to find backup folder OR create if not there
 backup = 'C:\\Users\\immer\\AppData\\Local\\VirtualStore\\Program Files (x86)\\Nethack\\Backup'
 os.chdir(playground)
+# TODO: cehck to see if file exists already, if not, don't copy.
+# TODO: use REGIX to find and copy all save files
 shutil.copy(playground + '\\immer-Dirk.NetHack-saved-game', backup)
+# TODO: use REGIX to find all save files
 shutil.copy(backup + '\\immer-Dirk.NetHack-saved-game', playground)
